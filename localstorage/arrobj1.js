8733eb7268f9d4492fc4b0999cc5656632c006ba
hothikieu2811 <hothikieu28112003@gmail.com>
2023-02-07 16:16:23 +0700

    update2

--- localstorage/arrobj1.js
+++ localstorage/arrobj1.js
@@ -4,28 +4,28 @@ var man = [
         name: "The Cosmo (Đen) Quần short kaki",
         code: "TC1025011BA",
         price: "250.000",
-        image: "/img/quan-kaki-nam-8.jpg",
+        image: "./img/quan-kaki-nam-8.jpg",
     },
     {
         id: 2,
         name: "Quần baggy đen sang trọng QQ",
         code: "TC1025011BA",
         price: "398.000",
-        image: "/img/quan-short.jpg",
+        image: "./img/quan-short.jpg",
     },
     {
         id: 3,
         name: "The Cosmo (Đen) Quần short kaki",
         code: "TC1025011BA",
         price: "250.000",
-        image: "/img/quan-den.jpg",
+        image: "./img/quan-den.jpg",
     },
     {
         id: 3,
         name: "The Cosmo (Đen) Quần short kaki",
         code: "TC1025011BA",
         price: "250.000",
-        image: "/img/quan-ngan.jpg"
+        image: "./img/quan-ngan.jpg"
     },
 ];
 var woment = [
@@ -34,27 +34,27 @@ var woment = [
         name: "Váy Fashion",
         code: "TC1025011BA",
         price: "250.000",
-        image: "/img/vay-jean.jpg",
+        image: "./img/vay-jean.jpg",
     },
     {
         id: 2,
         name: "Áo thun phối váy ngắn ",
         code: "TC1025011BA",
         price: "250.000",
-        image: "/img/vay-jean1.jpg",
+        image: "./img/vay-jean1.jpg",
     },
     {
         id: 3,
         name: "Áo khoác",
         code: "TC1025011BA",
         price: "300.000",
-        image: "/img/vay-nangtho.jpg",
+        image: "./img/vay-nangtho.jpg",
     },
     {
         id: 4,
         name: "Áo thun",
         code: "TC1025011BA",
         price: "250.000",
-        image: "/img/vay-xinh.jpg"
+        image: "./img/vay-xinh.jpg"
     },
 ];
